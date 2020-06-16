eb831c5e624eaf9a75db95676e85bc6590c3b99a
Helder Jose Melo <helder.goncalves@accurate.com.br>
2020-06-16 09:08:45 -0300

    axios progress bar

--- axios/vue-cli-new/src/components/HelloWorld.vue
+++ axios/vue-cli-new/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <p>aaaaaaaa</p>
+    <p>HelloWord.vue</p>
+    <p v-if="isShowProgress"> {{ frame }}% </p>
     <div v-html=ajaxResult> </div>
     <button @click="getTest()">Click</button>
   </div>
@@ -12,30 +13,70 @@
   export default {
     data: () => {
       return {
-        ajaxResult: ""
+        ajaxResult: "",
+        frame: 0,
+        isShowProgress: true,
+
+        frameInterval: null,
+        isFinish: false,
+        isStageOne: true,
+        isStageTwo: false,
+        isProgressFinish: false
       }
     },
     methods: {
       getTest: async function () {
-        console.log("aaaaaaaaaaaa")
+        console.log("início")
+
+        this.startInterval(10);
+
         let response = await axios.get(
           "http://localhost:3000/ajax-info",
           {
-            onUploadProgress:  function (progressEvent) {
-              console.log("onUploadProgress progressEvent: ", progressEvent)
-            },
-            onDownloadProgress:  function (progressEvent) {
-              console.log(Math.floor((progressEvent.loaded * 100) / progressEvent.total))
-              console.log(progressEvent.loaded, progressEvent.total)
-              console.log("onDownloadProgress progressEvent: ", progressEvent)
-            },
-
-
+            // onUploadProgress:  function (progressEvent) {
+            //   console.log("onUploadProgress progressEvent: ", progressEvent)
+            // },
+            // onDownloadProgress: function (progressEvent) {
+            //   console.log(Math.floor((progressEvent.loaded * 100) / progressEvent.total))
+            //   console.log(progressEvent.loaded, progressEvent.total)
+            //   console.log("onDownloadProgress progressEvent: ", progressEvent)
+            // },
           }
-          );
+        );
+
+        await new Promise((resolve) => {
+          if(this.isProgressFinish) {
+            resolve();
+          }
+        });
+
         console.log("fim")
-        console.log(response.data)
         this.ajaxResult = response.data
+      },
+
+      startInterval: function(intervalSpeedy) {
+        this.frameInterval = setInterval(() => {
+        this.frame++;
+
+          if(this.frame === 83) {
+            clearInterval(this.frameInterval);
+            setTimeout(() => {
+              this.isShowProgress = false;
+              this.isProgressFinish = true;
+            }, 500);
+          } else if (this.frame == 80) {
+            clearInterval(this.frameInterval);
+            this.startInterval(500)
+          }
+        }, intervalSpeedy);
+      },
+
+      stopInterval: function() {
+        clearInterval(this.frameInterval)
+      },
+
+      updateFrame: function (newValue) {
+        this.frame = newValue;
       }
     }
   }
